bd0b0d5e4906029f53126ba4db8c40aa0e4a83c6
DigruttolaGabriel <digruttolagabriel@gmail.com>
2021-07-16 21:29:31 -0300

    imports

--- src/application/controllers/categories.controller.ts
+++ src/application/controllers/categories.controller.ts
@@ -1,30 +1,17 @@
 import {
-  Body,
   Controller,
   Get,
   HttpCode,
   HttpStatus,
-  Param,
-  ParseIntPipe,
-  Post,
   UseGuards,
 } from '@nestjs/common';
-import { GetProductService } from '../../core/services/get.product.service';
-import { ProductDto } from '../../core/domain/dtos/product.dto';
-import { GetProductAdapter } from '../../infraestructure/adapters/usecase/get.product.adapter';
 import { AuthGuard } from '../guards/auth.guard';
 import {
   ApiBearerAuth,
-  ApiBody,
   ApiResponse,
   ApiTags,
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
-import { ProductHttpBody } from '../documentation/product.http.body';
-import { CreateProductAdapter } from 'src/infraestructure/adapters/usecase/create.product.adapter';
-import { CreateProductService } from '../../core/services/create.product.service';
-import { GetAllProductService } from '../../core/services/get.all.product.service';
-import { SearchProductsByCategoryService } from '../../core/services/search.products.by.category.service';
 import { Category } from '../../core/common/enums/category';
 
 @UseGuards(AuthGuard)
